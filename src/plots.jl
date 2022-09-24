10f223c283770d45ed8fd1e28b1f26c7762559c8
Miles Lucas <mdlucas@hawaii.edu>
2022-09-23 21:40:45 -1000

    remove SimpleOrbit recipe for now

--- src/plots.jl
+++ src/plots.jl
@@ -28,28 +28,3 @@ using RecipesBase
 
     return xs, ys
 end
-
-@recipe function f(orbit::SimpleOrbit; N = 90)
-    # We almost always want to see spatial coordinates with equal step sizes
-    aspect_ratio --> 1
-    # And we almost always want to reverse the RA coordinate to match how we
-    # see it in the sky.
-    xflip --> true
-    xguide --> "Δx"
-    yguide --> "Δy"
-
-    # We trace out in equal steps of true anomaly instead of time for a smooth
-    # curve, regardless of eccentricity.
-    νs = range(-π, π; length = N)
-    pos = @. _position(orbit, -orbit.aR_star, sin(νs), cos(νs))
-
-    xs = map(p -> p[1], pos)
-    ys = map(p -> p[2], pos)
-
-    if !isnothing(distance)
-        xs = @. xs / distance |> u"arcsecond"
-        ys = @. ys / distance |> u"arcsecond"
-    end
-
-    return xs, ys
-end
